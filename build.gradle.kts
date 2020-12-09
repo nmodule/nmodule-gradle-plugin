8ad0e6a3545a41d0abaaf2b21c8e463925c0b7d1
Lv Ming <lvming@restartech.com>
2020-12-09 08:31:44 +0800

    Update gradle to 6.8-rc-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import com.diffplug.spotless.LineEnding
 import java.nio.charset.Charset
 
 plugins {
-    kotlin("jvm") version "1.3.71"
+    kotlin("jvm") version embeddedKotlinVersion
     id("java-gradle-plugin")
     id("maven-publish")
     id("org.jetbrains.changelog") version "0.6.2"
-    id("com.diffplug.spotless") version "5.6.1"
+    id("com.diffplug.spotless") version "5.8.2"
     id("com.star-zero.gradle.githook") version "1.2.1"
     id("com.gradle.plugin-publish") version "0.12.0"
 }
@@ -15,7 +15,7 @@ group = "com.restartech"
 version = "0.2.0-SNAPSHOT"
 
 repositories {
-    maven("https://maven.aliyun.com/repository/central/")
+    // maven("https://maven.aliyun.com/repository/central/")
     mavenCentral()
 }
 
