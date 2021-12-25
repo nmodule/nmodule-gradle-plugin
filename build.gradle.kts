255457ec2a0bce2ff8719f0e56edf7775539e142
Lv Ming <lvming@restartech.com>
2021-12-25 19:44:52 +0800

    Downgrade gradle wrapper to 6.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "com.restartech"
-version = "0.3.1"
+version = "0.3.2"
 
 repositories {
     // maven("https://maven.aliyun.com/repository/central/")
--- src/main/kotlin/niagara/gradle/NiagaraModulePlugin.kt
+++ src/main/kotlin/niagara/gradle/NiagaraModulePlugin.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.language.jvm.tasks.ProcessResources
@@ -71,7 +71,7 @@ class NiagaraModulePlugin : Plugin<Project> {
         val sourcesJar = project.tasks.register("sourcesJar", Jar::class.java) { sourcesJar ->
             sourcesJar.archiveVersion.set("")
             sourcesJar.archiveClassifier.set("sources")
-            val main = project.extensions.getByType(JavaPluginExtension::class.java).sourceSets.named("main")
+            val main = project.convention.getPlugin(JavaPluginConvention::class.java).sourceSets.named("main")
             sourcesJar.from(main.get().allSource)
         }
 
