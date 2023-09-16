d3d38cd6e313ac8169d4bd92d22280fdec8b7276
lvming <lvming@restartech.com>
2023-09-16 11:03:05 +0800

    Update gradle to 8.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.6.0]
+### Changed
+- Update gradle to 8.2.1
+
 ## [0.5.1]
 ### Changed
 - Change log level to info
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.restartech"
-version = "0.5.1"
+version = "0.6.0"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public/")
