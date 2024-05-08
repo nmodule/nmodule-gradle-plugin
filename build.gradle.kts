bd6c8ecc02474a788d40910a2851f7b654c57365
lvming <lvming@restartech.com>
2024-05-08 16:08:10 +0800

    Update gradle to 8.7; com.gradle.plugin-publish to 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.24"
     id("java-gradle-plugin")
     id("maven-publish")
     id("org.jetbrains.changelog") version "1.3.1"
-    id("com.diffplug.spotless") version "6.22.0"
+    id("com.diffplug.spotless") version "6.25.0"
     id("com.star-zero.gradle.githook") version "1.2.1"
-    id("com.gradle.plugin-publish") version "0.18.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "com.restartech"
-version = "0.7.1"
+version = "0.8.0"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public/")
@@ -20,24 +20,22 @@ dependencies {
 }
 
 gradlePlugin {
+    website = "https://github.com/nmodule/nmodule-gradle-plugin"
+    vcsUrl = "https://github.com/nmodule/nmodule-gradle-plugin"
+
     plugins.create("niagaraModulePlugin") {
         id = "com.restartech.nmodule"
         displayName = "Niagara Module Gradle Plugin"
         description = "A gradle plugin to build Niagara module in Kotlin"
         implementationClass = "niagara.gradle.NiagaraModulePlugin"
+        tags = listOf("niagara", "nmodule", "kotlin")
     }
-}
-
-pluginBundle {
-    website = "https://github.com/nmodule/nmodule-gradle-plugin"
-    vcsUrl = "https://github.com/nmodule/nmodule-gradle-plugin"
-    tags = listOf("niagara", "nmodule", "kotlin")
 }
 
 spotless {
     lineEndings = com.diffplug.spotless.LineEnding.UNIX
 
-    val ktlintVersion = "1.0.1"
+    val ktlintVersion = "1.2.1"
 
     val editorConfigOverride = mapOf(
         "ij_kotlin_allow_trailing_comma" to true,
@@ -63,7 +61,7 @@ spotless {
 githook {
     hooks {
         create("pre-commit") {
-            shell = "./gradlew check"
+            task = "check"
         }
     }
 }
